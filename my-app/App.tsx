9b5b903293dbdeedf913da7e65c88ff75bea2c38
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-12-29 19:11:50 +0100

    s

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>LE COUSCOUS C'test UN BANGER</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
